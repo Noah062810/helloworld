e712ab8f6b85458efb77bf57a05aea1e3cfdb477
Noah062810 <122543656+Noah062810@users.noreply.github.com>
2023-01-13 09:54:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
+    basic.showString("Hello!")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
+    basic.showString("I'm AI AKA AIden")
 })
 basic.showIcon(IconNames.Yes)
